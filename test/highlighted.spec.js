5e328cb7f6b9887a108a70a10d012a68b52c83d7
lol3rrr <s.loler03@gmail.com>
2021-03-12 21:16:21 +0100

    Fixed the tests

--- test/highlighted.spec.js
+++ test/highlighted.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Highlight from '@/components/highlight.vue'
 
-describe('Logo', () => {
+describe('Highlight', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Highlight)
     expect(wrapper.vm).toBeTruthy()
   })
 })
